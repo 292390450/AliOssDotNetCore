a6c3b5eccc3c49f0bbe394471d08b3171ae39287
Administrator <Administrator@MicroWin10-1218>
2018-05-25 15:22:11 +0800

    sa

---
+++ OssTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Aliyun.OSS;
+
+namespace OssTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            OssClient client=new OssClient("","","");
+        }
+    }
+}
